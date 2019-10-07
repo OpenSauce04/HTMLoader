81ad1a4710720253d09374da3b4d5ccffe42e2a1
Daniel-Bradbury <danieljamesbradbury@hotmail.com>
2019-10-07 19:31:57 +0100

    Fixed linux firefox water colour

---
+++ menu/correctCSS.js
@@ -0,0 +1,26 @@
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+console.log(OSName)
+if ((OSName=="Linux") || (OSName=="UNIX")) {
+	console.log("Detected Unix")
+	document.write("<style>");
+	document.write("body {");
+	document.write("	background-color: #7de4ff;");
+	document.write("}");
+	document.write("</style>");
+} else {
+	document.write("<style>");
+	document.write("body {");
+	document.write("	background-color: #7de4ff;");
+	document.write("}");
+	document.write("@-moz-document url-prefix() {");
+	document.write("body {");
+	document.write("	background-color: #88FFFF;");
+	document.write("}");
+	document.write("}");
+	document.write("</style>");
+}
