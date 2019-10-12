01e84a42947c9030b55c57034eb2315208a61283
Daniel-Bradbury <danieljamesbradbury@hotmail.com>
2019-10-12 18:50:39 +0100

    Fixed incorrect icon loading issue

--- menu/loadMenu.js
+++ menu/loadMenu.js
@@ -18,23 +18,41 @@ function readTextFile(file)
     }
     rawFile.send(null);
 }
+function readTextFileAlt(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allTextAlt = rawFile.responseText;
+                
+            }
+        }
+    }
+    rawFile.send(null);
+}
 var i = 0
 while (true) {
 i = i + 1
 allText=null
 readTextFile(`../apps/${i}/index.html`)
 if (allText != null) {
+allTextAlt=null
 allText=null
-readTextFile(`../apps/${i}/icon.png`)
+readTextFileAlt(`../apps/${i}/NAME`)
+readTextFile(`../apps/${i}/${allTextAlt}.icon.png`)
 if (allText != null) {
 allText=null
-readTextFile(`../apps/${i}/NAME`)
 document.write("<br><br>")
-document.write(`<appext></appext><app><a href=../apps/${i}/index.html><img src="../apps/${i}/icon.png" width=20px height=20px> </img> ${allText}</a></app>`);
+document.write(`<appext></appext><app><a href=../apps/${i}/index.html><img src="../apps/${i}/${allTextAlt}.icon.png" width=20px height=20px> </img> ${allTextAlt}</a></app>`);
 } else {
 readTextFile(`../apps/${i}/NAME`)
 document.write("<br><br>")
-document.write(`<appext></appext><app><a href=../apps/${i}/index.html><img src="deficon.png" width=20px height=20px> </img> ${allText}</a></app>`);
+document.write(`<appext></appext><app><a href=../apps/${i}/index.html><img src="deficon.png" width=20px height=20px> </img> ${allTextAlt}</a></app>`);
 }
 } else {
   elem.remove();
--- patch.rb
+++ patch.rb
@@ -8,7 +8,11 @@ apps.each do |app|
     puts "  #{app}"
     i+=1
     FileUtils.copy_entry("./apps/#{app}", "./patched/#{i}", preserve = false, dereference = false)
+    if File.file?("./patched/#{i}/icon.png") then
+    	File.rename("./patched/#{i}/icon.png", "./patched/#{i}/#{apps[i-1]}.icon.png")
+	end
 	file = File.open("./patched/#{i}/NAME", "w")
-	file.puts "#{app}"
+	file.print "#{app}"
+	STDOUT.flush();
 	file.close
 end
