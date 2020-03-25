1feec0f61df971a5d12b29f14df1e6bd0a05d460
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-03-25 13:31:06 +0000

    Implemented app renaming

--- update.rb
+++ update.rb
@@ -3,18 +3,28 @@ require 'fileutils'
 puts "Removing old apps..."
 FileUtils.rm_rf("HTMLoader-apps")
 FileUtils.rm_rf("apps")
-puts "Downloading..."
+puts "Downloading main repo..."
 Git.clone("https://github.com/Daniel-Bradbury/HTMLoader-apps.git","HTMLoader-apps", :branch => "better-download")
 Dir.chdir "HTMLoader-apps"
 urlsf=File.open('appurls.txt')
 urls=urlsf.read
 urlsf.close()
-urls.gsub!(/\r\n?/, "\n")
+puts "Downloading apps..."
 urls.each_line do |line|
+    puts "  Downloading "+line.chomp+"..."
     Git.clone(line.chomp,File.basename(line.chomp,".git"))
+end
+puts "Renaming folders to app name..."
+namesf=File.open('appnames.txt')
+names=namesf.read
+namesf.close()
+names.gsub!(/\r\n?/, "\n")
+names.each_line do |line|
+    File.rename(line.chomp.split("|")[0], line.chomp.split("|")[1]) 
 end
 puts "Preparing patch..."
 File.delete("appurls.txt")
+File.delete("appnames.txt")
 Dir.chdir "../"
 FileUtils.mv("HTMLoader-apps", "apps")
 FileUtils.rm_rf("apps/.git")
