9e45e5a298ff4e2e00be4499b426b4863c5641ec
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-03-25 11:54:04 +0000

    Implemented improved download system

--- update.rb
+++ update.rb
@@ -1,12 +1,22 @@
 require 'git'
 require 'fileutils'
-puts "Removing old entries..."
+puts "Removing old apps..."
 FileUtils.rm_rf("HTMLoader-apps")
 FileUtils.rm_rf("apps")
 puts "Downloading..."
-Git.clone("https://github.com/Daniel-Bradbury/HTMLoader-apps.git","HTMLoader-apps")
+Git.clone("https://github.com/Daniel-Bradbury/HTMLoader-apps.git","HTMLoader-apps", :branch => "better-download")
+Dir.chdir "HTMLoader-apps"
+urlsf=File.open('appurls.txt')
+urls=urlsf.read
+urlsf.close()
+urls.gsub!(/\r\n?/, "\n")
+urls.each_line do |line|
+    Git.clone(line.chomp,File.basename(line.chomp,".git"))
+end
 puts "Preparing patch..."
-File.rename("HTMLoader-apps", "apps")
+File.delete("appurls.txt")
+Dir.chdir "../"
+FileUtils.mv("HTMLoader-apps", "apps")
 FileUtils.rm_rf("apps/.git")
 system("ruby patch.rb")
 puts "Finishing update..."
