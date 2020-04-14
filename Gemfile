efeee3667d0f2dc3e34ed42e87215ac4688b0ebb
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-04-14 16:47:11 +0100

    Added --connect argument

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'fileutils', '1.2.0'
 gem 'git', '1.5.0'
+gem "launchy", "~> 2.5"
--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'webrick'
+require 'launchy'
 server = WEBrick::HTTPServer.new :Port => 8000
 server.mount "/", WEBrick::HTTPServlet::FileHandler, './'
 trap('INT') { server.stop }
+if ARGV[0]=="--connect"
+  Launchy.open("http://localhost:8000")
+end
 server.start
